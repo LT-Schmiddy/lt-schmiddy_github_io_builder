4e6025ed9310d50851c844fe9bd0e46bce56d1a4
LT-Schmiddy <schmidalex1996@gmail.com>
2026-03-22 22:41:50 -0400

    site: adding repy page

--- build_site.py
+++ build_site.py
@@ -11,11 +11,17 @@ def main():
     rsrc_root = site_root.joinpath("rsrc")
     b = MainBuilder(proot, template_root, markdown_root, site_root, rsrc_root)
     
-    
     b.add_style(proot.joinpath("./src/sass/main.scss"), "main.css");
     b.add_script(proot.joinpath("./src/ts/main.ts"), "bundle.js");
     
     b.add_page("index.html", "index.html", "Home", title="Home", desc="LT_Schmiddy's Stash")
+    
+    b.add_page("markdown_page.html", "n64r_repy.html", "N64Recompiled/REPY",
+        title="EZTR", desc="RecompExternalPython for N64Recompiled",
+        jinja_args={
+            "markdown_doc": "REPY.md"
+        }
+    )
     
     b.add_page("markdown_page.html", "z64r_eztr.html", "Zelda64Recompiled/EZTR",
         title="EZTR", desc="EZ Text Replacer for Zelda64Recompiled",
---
+++ src/markdown/REPY.md
@@ -0,0 +1,5 @@
+# REPY for N64Recompiled
+
+RecompExternalPython is major extension library for the N64Recompiled ecosystem.
+
+The documentation for the library can be found [here](./docs/REPY_for_N64Recomp/index.html).
